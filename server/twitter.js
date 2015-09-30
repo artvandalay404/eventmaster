60c5e3bca125658c5177be2844d3bf65469e1e8f
Michael Lin <nagn@users.noreply.github.com>
2015-09-29 22:55:36 -0400

    Update twitter.js

--- server/twitter.js
+++ server/twitter.js
@@ -2,8 +2,8 @@ var Twit = require('twit')
 
 exports.getData = function (latitude, longitude, range, callback) {
   var T = new Twit({
-    consumer_key: 'SBaf2q9o4CZeZIZ67UDflCbgb',
-    consumer_secret: 'GOy85nawC5U0R6YtbwPlgSyifytXrTjfVFZHDzJRqTpZaS0oLF',
+    consumer_key: '',
+    consumer_secret: '',
     app_only_auth: true
   })
   var geocodeString = latitude + "," + longitude + "," + range + "mi";
